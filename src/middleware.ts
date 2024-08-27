4e154d73a7553e84596365ae4d3936f6c5696541
chromecodes <hameedhussain782@gmail.com>
2024-08-27 11:34:24 +0530

    issue fixed

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,7 +4,7 @@ import type { NextRequest } from "next/server";
 
 // This function can be marked `async` if using `await` inside
 export function middleware(request: NextRequest) {
-  const session = useSession();
+  // const session = useSession();
 
   let currentPath = request.nextUrl.pathname;
 
